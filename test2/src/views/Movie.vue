42e9946097294c5e7486f6cd21e59619ae9a2529
zhchuanger <2209796387@qq.com>
2019-10-02 19:02:32 +0800

    test2

--- test2/src/views/Movie.vue
+++ test2/src/views/Movie.vue
@@ -13,6 +13,7 @@
                 </div>
             </li>
         </ul>
+        <img v-show="isshow" class="loading" src="@/assets/images/loading.gif" alt="">
 
     </div>
 </template>
@@ -22,24 +23,48 @@
     export default {
         data() {
             return {
-                movieList:[]
-                
+                movieList:[],
+                isshow : false,
+                flag:true
             }
         },
         created() {
-            //跨域 域名(baidu.com) 端口(8080 8081) 协议(http https)
-            //有一个不同即为跨域，都相同才是同源
-            // 解决跨域问题：第三方服务器代理 jsonbird  https://bird.ioliu.cn/v1?url=
-            axios.get('https://bird.ioliu.cn/v1?url=https://douban.uieee.com/v2/movie/in_theaters?start=0&count=10')
-            // 成功走then 失败走catch
-            .then((res)=>{
-                console.log(res.data);
-                this.movieList = res.data.subjects;
-
-            }).catch((res)=>{
-                console.log(res);
+            this.getMovieList(this.movieList.length);
+            window.onscroll = ()=> {
+                let scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+                let clientHeight = document.documentElement.clientHeight;
+                let scrollHeight = document.documentElement.scrollHeight;
+                if(Math.abs(scrollHeight-clientHeight-scrollTop)<1){
+                    this.getMovieList(this.movieList.length);
+                    
+                }
                 
-            })
+            }
+        },
+        methods: {
+            getMovieList(num){
+                    //跨域 域名(baidu.com) 端口(8080 8081) 协议(http https)
+                    //有一个不同即为跨域，都相同才是同源
+                    // 解决跨域问题：第三方服务器代理 jsonbird  https://bird.ioliu.cn/v1?url=
+                    this.isshow = true;
+                    if(this.flag){
+                        this.flag = false;//当前已经是发送请求状态 不可以继续请求
+                        axios.get('https://bird.ioliu.cn/v1?url=https://douban.uieee.com/v2/movie/in_theaters?start='+num+'&count=10')
+                        // 成功走then 失败走catch
+                        .then((res)=>{
+                            console.log(res.data);
+                            this.movieList = [...this.movieList,...res.data.subjects];
+                            this.isshow =false;
+                            this.flag = true;//请求成功后 可继续请求
+
+                        }).catch((res)=>{
+                            console.log(res);
+                            
+                        })
+
+                    }
+                    
+            }
         },
         
     }
@@ -58,6 +83,14 @@
         border-bottom:1px solid #000;
     }
 
+}
+.loading{
+    width: 1rem;
+    height: 1rem;
+    position: fixed;
+    left:50%;
+    top:50%;
+    transform:translate(-50%,-50%);
 }
 
 
